1346b35fd2cbdad191f5a1482d7abd7da2bc45a7
IfthiarHosain <iftiar2@outlook.com>
2024-12-25 00:57:09 +0600

    time_formate.py

--- time_formate.py
+++ time_formate.py
@@ -1,4 +1,6 @@
 import time
 current_time=time.localtime()
 formatted_time=time.strftime("%I:%M:%S:%p",current_time)
+#formatted_time=time.strftime("%H:%M:%S", current_time) 
+# for 24 hour clock formate
 print("current time:", formatted_time)
